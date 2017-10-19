302e04932827a3b751955917bb99a0de46d75cbb
RYZEN51600-ZEYN\Ataberk <ata.palacioglu@gmail.com>
2017-10-20 00:18:18 +0300

    updated

--- Lecture03/Assets/Scripts/Controller.cs
+++ Lecture03/Assets/Scripts/Controller.cs
@@ -19,6 +19,6 @@ public class Controller : MonoBehaviour
         }
 
         // Second approach
-        // MessageSystem.TriggerModifyButtonClicked();
+        MessageSystem.TriggerModifyButtonClicked();
     }
 }
